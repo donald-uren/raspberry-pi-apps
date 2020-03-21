c0377f1cf3540f76bdd4c33ab99a7eee1edaa43c
Donald U'Ren <s3686963@student.rmit.edu.au>
2020-03-21 17:56:43 +1100

    - added thread for background running of task

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from threading import Thread
 from monitorAndDisplay import TemperatureDisplay
 import time
 
+stop = None
 display = TemperatureDisplay()
-display._run()
-print('type \'stop\' to halt program.')
+display_thread = Thread(target=display._run())
+while stop != "stop":
+    stop = input("enter \'stop\' to halt program")
+display._terminate()
 
