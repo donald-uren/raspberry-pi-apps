10474ef3d659cc3aeffcf7110a24fa794fd6c16d
Daniel Dao <guangdanny@gmail.com>
2020-03-28 23:05:12 +1000

    electronicdie

---
+++ electronicDie.py
@@ -0,0 +1,106 @@
+from sense_emu import SenseHat
+from time import sleep
+import random
+
+class ElectronicDice:
+    def __init__(self):
+        self.__sense = SenseHat()
+
+    def roll(self):
+        x = (255, 255, 255)
+        o = (0, 0, 0)
+        options = [
+            [
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+            ],
+            [
+                o, o, o, o, o, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, o, o, o, o, o,
+            ],
+            [
+                x, x, o, o, o, o, o, o,
+                x, x, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, x, x,
+                o, o, o, o, o, o, x, x,
+            ],
+            [
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+            ],
+            [
+                o, o, o, o, o, o, o, o,
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+                o, o, o, o, o, o, o, o,
+                o, o, o, o, o, o, o, o,
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+                o, o, o, o, o, o, o, o,
+            ],
+            [
+                o, o, o, o, o, o, o, o,
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+                o, o, o, x, x, o, o, o,
+                o, o, o, x, x, o, o, o,
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+                o, o, o, o, o, o, o, o,
+            ],
+            [
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+                o, o, o, o, o, o, o, o,
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+                o, o, o, o, o, o, o, o,
+                o, x, x, o, o, x, x, o,
+                o, x, x, o, o, x, x, o,
+            ],
+        ]
+        
+        X = 0
+        Y = 0
+        Z = 0
+
+        while True:
+            x, y, z = self.__sense.get_accelerometer_raw().values()
+            x = abs(x)
+            y = abs(y)
+            z = abs(z)
+            
+            if (x != X or y != Y or z != Z):
+                for i in range(6):
+                    self.__sense.set_pixels(random.choice(options))
+                    sleep(0.5)
+                    self.__sense.clear()
+                return self.__sense.set_pixels(random.choice(options))
+
+            X = x
+            Y = y
+            Z = z                
+
+        
