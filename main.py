2fa52061a668ebef2f9df006dacc031f6c10018f
donald-uren <s3686963@student.rmit.edu.au>
2020-04-03 16:37:50 +1100

    minor change to main: run_program function

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ Main/driver class to run/halt program
 from threading import Thread
 from monitorAndDisplay import TemperatureDisplay
 
-display = TemperatureDisplay("config.json")
-display_thread = Thread(target=display.run)
-display_thread.start()
 
-stop = None
-while stop != "stop":
-    stop = input("enter \'stop\' to halt program.\n")
+def run_program():
+    display = TemperatureDisplay("config.json")
+    display_thread = Thread(target=display.run)
+    display_thread.start()
 
-display.terminate()
+    stop = None
+    while stop != "stop":
+        stop = input("enter \'stop\' to halt program.\n")
+
+    display.terminate()
+
+
+run_program()
