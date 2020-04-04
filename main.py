9a352eaae6bf27eaeef7a137df4c7de43efdb485
donald-uren <s3686963@student.rmit.edu.au>
2020-04-04 16:32:28 +1100

    testing joystick stop/start for temperature application

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Main/driver class to run/halt program
 
 from threading import Thread
 from monitorAndDisplay import TemperatureDisplay
+from sense_hat import SenseHat, ACTION_PRESSED
 
 
 def run_program():
@@ -11,9 +12,12 @@ def run_program():
     display_thread = Thread(target=display.run)
     display_thread.start()
 
-    stop = None
-    while stop != "stop":
-        stop = input("enter \'stop\' to halt program.\n")
+    stop = False
+    sense = SenseHat()
+    while stop is False:
+        events = sense.stick.get_events()
+        for event in events:
+            stop = True if event.action == ACTION_PRESSED else False
 
     display.terminate()
 
