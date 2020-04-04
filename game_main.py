3bd39b4c8f04cb9a3504cbf8a34cbe86f8832e91
donald-uren <s3686963@student.rmit.edu.au>
2020-04-04 20:17:01 +1100

    bug fixing game.py: ImportErrors

---
+++ game_main.py
@@ -0,0 +1,9 @@
+from dice_game.game import Game
+
+"""
+Start a game with 2 parameters:
+    - A list of players and.
+    - A csv file name to export the result to
+"""
+game = Game(["John", "Mike"], "winner.csv")
+game.run()
