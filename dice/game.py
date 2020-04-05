be123716d46a5300262f993261a7eb77d1fa371d
donald-uren <s3686963@student.rmit.edu.au>
2020-04-05 18:35:12 +1000

    updated winner.csv filepath

--- dice/game.py
+++ dice/game.py
@@ -114,5 +114,5 @@ Start a game with 2 parameters:
     - A list of players and.
     - A csv file name to export the result to
 """
-game = Game(["John", "Mike"], "winner.csv")
+game = Game(["John", "Mike"], "dice/winner.csv")
 game.run()
